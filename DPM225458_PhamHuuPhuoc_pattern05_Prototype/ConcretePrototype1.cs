a470bb568ed301159622e985bb05dc7bee2fd121
Phuoc121 <phuoc_dpm225458@student.agu.edu.vn>
2025-10-06 10:15:41 +0700

    Add files via upload

---
+++ DPM225458_PhamHuuPhuoc_pattern05_Prototype/ConcretePrototype1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225458_PhamHuuPhuoc_pattern05_Prototype
+{
+    public class ConcretePrototype1 : Prototype
+    {
+    
+        public ConcretePrototype1(string id)
+            : base(id)
+        {
+        }
+        
+        public override Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+    }
+}
---
+++ DPM225458_PhamHuuPhuoc_pattern06_Singleton/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225458_PhamHuuPhuoc_pattern06_Singleton
+{
+    public class Singleton
+    {
+        static Singleton instance;
+        // Constructor is 'protected'
+        protected Singleton()
+        {
+        }
+        public static Singleton Instance()
+        {
+            // Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+
+}
