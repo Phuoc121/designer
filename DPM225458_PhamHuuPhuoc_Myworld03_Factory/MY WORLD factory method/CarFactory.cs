78ebec9516ac2afd15af521d2c49b07667fc5fa8
Phuoc121 <phuoc_dpm225458@student.agu.edu.vn>
2025-10-06 10:53:42 +0700

    Add files via upload

---
+++ DPM225458_PhamHuuPhuoc_Myworld03_Factory/MY WORLD factory method/CarFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Myworld03_Factory.Vehicle;
+
+namespace Myworld03_Factory
+{
+
+        public class CarFactory : VehicleFactory
+        {
+            public override void CreateVehicle()
+            {
+                Vehicle = new Car();
+                Vehicle.Features.Add("4 wheels");
+                Vehicle.Features.Add("Air conditioning");
+                Vehicle.Features.Add("Radio");
+            }
+        }
+    }
+
---
+++ DPM225458_PhamHuuPhuoc_Myworld03_Factory/MY WORLD factory method/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myworld03_Factory
+{
+
+        public abstract class Vehicle
+        {
+            public List<string> Features = new List<string>();
+
+            public void ShowFeatures()
+            {
+                Console.WriteLine(GetType().Name + " features:");
+                foreach (var feature in Features)
+                {
+                    Console.WriteLine(" - " + feature);
+                }
+            }
+        }
+
+        // Concrete Products
+        public class Car : Vehicle
+        {
+        }
+
+        public class Bike : Vehicle
+        {
+        }
+
+        public class Truck : Vehicle
+        {
+        }
+    }
+
