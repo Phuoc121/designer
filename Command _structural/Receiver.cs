c29442b126500f4df6c46ffc1e8c2ca06ebb3e38
Phuoc121 <phuoc_dpm225458@student.agu.edu.vn>
2025-10-06 08:30:46 +0700

    Add files via upload

---
+++ Command _structural/Receiver.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// The 'Receiver' class
+/// </summary>
+public class Receiver
+{
+    public void Action()
+    {
+        Console.WriteLine("Called Receiver.Action()");
+    }
+}
