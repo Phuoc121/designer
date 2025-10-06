a63c7850a80a80e0daf139cbe82695c28c4eeaeb
Phuoc121 <phuoc_dpm225458@student.agu.edu.vn>
2025-10-06 09:37:17 +0700

    Add files via upload

---
+++ Pattern01_AbstractFactory/ConcreteFactory1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern01_AbstractFactory
+{
+    public class ConcreteFactory1 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA() => new ProductA1();
+        public override AbstractProductB CreateProductB() => new ProductB1();
+    }
+}
---
+++ Pattern01_AbstractFactory/ConcreteFactory2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern01_AbstractFactory
+{
+    public class ConcreteFactory2 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA() => new ProductA2();
+        public override AbstractProductB CreateProductB() => new ProductB2();
+    }
+}
