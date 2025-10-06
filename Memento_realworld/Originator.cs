60318e482763f51cdaefacee38d87f74e62ea3c8
Phuoc121 <phuoc_dpm225458@student.agu.edu.vn>
2025-10-06 08:36:31 +0700

    Add files via upload

---
+++ Memento_realworld/Originator.cs
@@ -0,0 +1,27 @@
+﻿/// <summary>
+/// The 'Originator' class
+/// </summary>
+public class Originator
+{
+    string state;
+    public string State
+    {
+        get { return state; }
+        set
+        {
+            state = value;
+            Console.WriteLine("State = " + state);
+        }
+    }
+    // Creates memento 
+    public Memento CreateMemento()
+    {
+        return (new Memento(state));
+    }
+    // Restores original state
+    public void SetMemento(Memento memento)
+    {
+        Console.WriteLine("Restoring state...");
+        State = memento.State;
+    }
+}
---
+++ Memento_realworld/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Originator o = new Originator();
+            o.State = "On";
+            // Store internal state
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
+            // Continue changing originator
+            o.State = "Off";
+            // Restore saved state
+            o.SetMemento(c.Memento);
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
